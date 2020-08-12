19aefb774d3fec558fefa8ae5fc17c6d7005b66e
bashvtini <bashvtini@gmail.com>
2020-08-12 11:39:27 +0200

    Updates

---
+++ precache-manifest.eea05d8e47c8bb6f099fb61d8d3d01d4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555a4bd6150409e3639e757672bb9af7",
+    "url": "/punetori-ui/index.html"
+  },
+  {
+    "revision": "3aeb3971e5ab09a9b009",
+    "url": "/punetori-ui/static/css/main.19c28b83.chunk.css"
+  },
+  {
+    "revision": "5c1abf7afa21139e20ca",
+    "url": "/punetori-ui/static/js/2.1aae2980.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/punetori-ui/static/js/2.1aae2980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aeb3971e5ab09a9b009",
+    "url": "/punetori-ui/static/js/main.31bc1852.chunk.js"
+  },
+  {
+    "revision": "4c96ee98a378f545e160",
+    "url": "/punetori-ui/static/js/runtime-main.b2236734.js"
+  },
+  {
+    "revision": "6f770f93df654b93a60146c84d7ff8de",
+    "url": "/punetori-ui/static/media/bookmark.6f770f93.svg"
+  },
+  {
+    "revision": "60923ccb6e2ba8d4553cccbb31e12d5a",
+    "url": "/punetori-ui/static/media/eye-off.60923ccb.svg"
+  },
+  {
+    "revision": "5380c6d1870d48b5b7229c106a1fbcf6",
+    "url": "/punetori-ui/static/media/eye.5380c6d1.svg"
+  },
+  {
+    "revision": "15028421339a8585c49f38c4be4217d4",
+    "url": "/punetori-ui/static/media/menu.15028421.svg"
+  },
+  {
+    "revision": "bdbd9ca9c6ff17d9ac97800ba92336e2",
+    "url": "/punetori-ui/static/media/search.bdbd9ca9.svg"
+  }
+]);
