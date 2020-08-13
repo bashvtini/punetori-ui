a2d61a1e1317680f08f83c42e3b44955dec0f8cb
bashvtini <bashvtini@gmail.com>
2020-08-13 12:09:12 +0200

    jobs table css bug

--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -7,34 +7,37 @@ export default function Results() {
   );
 
   return (
-    <div id="search-results">
+    <div id="search-results" className="job-table">
       {searchLoading ? (
         <div className="loading">
           <div className="loader"></div>
         </div>
       ) : null}
+      <div className="jobs-wrapper">
+        {searchError ? (
+          <h1 style={{ textAlign: "center" }}>Nothing was found</h1>
+        ) : null}
 
-      {searchError ? (
-        <h1 style={{ textAlign: "center" }}>Nothing was found</h1>
-      ) : null}
+        {jobs.length !== 0 ? (
+          <React.Fragment>
+            <div className="results-header">
+              <p>{query.charAt(0).toUpperCase() + query.slice(1)} in Albania</p>
+              <p className="jobs-no">{count} Jobs</p>
+            </div>
 
-      {jobs.length !== 0 ? (
-        <React.Fragment>
-          <div className="results-header">
-            <p>{query.charAt(0).toUpperCase() + query.slice(1)} in Albania</p>
-            <p className="jobs-no">{count} Jobs</p>
-          </div>
-
-          <ul className="jobs">
-            {jobs.map((job, index) => (
-              // eslint-disable-next-line
-              <a href={job.link} key={index} target="_blank">
-                <p>{job.title}</p>
-              </a>
-            ))}
-          </ul>
-        </React.Fragment>
-      ) : null}
+            <ul className="jobs">
+              {jobs.map((job, index) => (
+                <li>
+                  {/* eslint-disable-next-line */}
+                  <a href={job.link} key={index} target="_blank">
+                    <p>{job.title}</p>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        ) : null}
+      </div>
     </div>
   );
 }
